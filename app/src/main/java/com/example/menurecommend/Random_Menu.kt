b55effcb904d9f023b549d181e318fc1efa7deb5
박승호 <joinus0409@naver.com>
2023-07-25 22:08:27 +0900

    main 인터페이스 xml, main에서 랜덤 추천 화면으로 이동 구현

---
+++ app/src/main/java/com/example/menurecommend/Random_Menu.kt
@@ -0,0 +1,11 @@
+package com.example.menurecommend
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Random_Menu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_random_menu)
+    }
+}
