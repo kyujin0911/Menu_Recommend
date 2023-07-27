3b86e7ca59fa224d316c33722c9d00fcc9567f52
박승호 <joinus0409@naver.com>
2023-07-27 15:30:50 +0900

    Map View 추가 네이버 지도와 연동하여 주소와 일치하는 위치 표시 Dataset에 경위도를 추가하여 지도에 마커 표시

---
+++ app/src/main 2/java 2/com/example/menurecommend/MapView.kt
@@ -0,0 +1,50 @@
+package com.example.menurecommend
+
+import android.content.Context
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.example.menurecommend.databinding.ActivityMapViewBinding
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMapSdk
+import com.naver.maps.map.overlay.InfoWindow
+import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.util.MarkerIcons
+
+class MapView : AppCompatActivity() {
+
+    val TAG = "latilong"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map_view)
+
+        val name = intent.getStringExtra("name")!!
+        val lati = intent.getDoubleExtra("lati", 0.0)
+        val long = intent.getDoubleExtra("long", 0.0)
+        val marker = Marker()
+        val cameraUpdate = CameraUpdate.scrollTo(LatLng(lati, long))
+        val fm = supportFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.navermap) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.navermap, it).commit()
+            }
+
+
+
+        mapFragment.getMapAsync { naverMap ->
+            Log.d(TAG, "${lati},${long}")
+            marker.position = LatLng(lati, long)
+            marker.icon = MarkerIcons.BLACK
+            marker.iconTintColor = Color.RED
+            marker.captionText = name
+            marker.captionTextSize = 15f
+            marker.map = naverMap
+            naverMap.moveCamera(cameraUpdate)
+
+        }
+
+    }
+}
